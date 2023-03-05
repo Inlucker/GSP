f63f9739713cf5389715479ac1937c4cd61ddc69
Inlucker <arseny707@mail.ru>
2023-03-05 14:05:04 +0300

    added sql file

---
+++ GSP_adapt/test.sql
@@ -0,0 +1,27 @@
+select *
+from logs
+where command = "Arc";
+
+select command, (row_number() over (partition by same))-1 as cmd_id
+from(
+    select 1 as same, command
+    from logs
+    group by command);
+
+with cmds_ids(command, cmd_id) as
+(
+select command, (row_number() over (partition by same))-1 as cmd_id
+    from
+    (
+        select 1 as same, command
+        from logs
+        group by command
+    )
+)
+select id, session_id, time, logs.command, cmd_id
+from cmds_ids join logs on (cmds_ids.command = logs.command);
+
+
+select command, count(command)
+from logs
+group by command;
