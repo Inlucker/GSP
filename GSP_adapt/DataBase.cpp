ab6ac8d5a4ed38b8c74db00bedc074b05603445c
Inlucker <Arseny707@mail.ru>
2023-05-11 20:35:43 +0300

    fixed rows order in getAllLogs

--- GSP_adapt/DataBase.cpp
+++ GSP_adapt/DataBase.cpp
@@ -184,7 +184,8 @@ Status DataBase::getAllLogs(int commands_num, QList<Session> &sessions)
                         )\
                       )\
                       select id, session_id, int_time, cmd_id\
-                      from cmds_ids join logs on (cmds_ids.command = logs.command);";
+                      from cmds_ids join logs on (cmds_ids.command = logs.command)\
+                      order by id, session_id, int_time;";
 
   Status s = execQuery(query_str);
   if (s != OK)
